b2610944602f0ff9282d4f7ef2009c034e06f74a
Nishant Jaiswal <nj19944@gmail.com>
2021-01-29 02:53:10 +0530

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const forecast = (latitude , longitude , callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=f0a0506ef3e25db1c1ab195d20d86705&query=' + latitude + ',' + longitude  ;
+
+    request({url,json : true} , (error , response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            console.log(response.body);
+            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+        } 
+    })
+}
+
+module.exports = forecast;
